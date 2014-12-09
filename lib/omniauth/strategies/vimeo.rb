15fe6f6d7633f758f352df9ef09378c9ce6c67a3
Peter Gerard <peter@accidental.tv>
2014-12-09 08:17:35 -0500

    simplify

--- lib/omniauth/strategies/vimeo.rb
+++ lib/omniauth/strategies/vimeo.rb
@@ -12,38 +12,6 @@ module OmniAuth
         :site => "https://api.vimeo.com"
       }
 
-      uid { user_info['id'] }
-
-      info do
-        {
-          'nickname' => user_info['username'],
-          'name' => user_info['display_name'],
-          'location' => user_info['location'],
-          'description' => user_info['bio'],
-          'image' => user_info['portraits']['portrait'].select{|h| h['height'] == '300'}.first['_content'],
-          'urls' => {
-            'website' => user_info['url'],
-            'vimeo' => user_info['profileurl']
-          }
-        }
-      end
-
-      extra do
-        {
-          :raw_info => raw_info
-        }
-      end
-
-      def user_info
-        @user_info ||= raw_info.nil? ? {} : raw_info["person"]
-      end
-
-      def raw_info
-        @raw_info ||= MultiJson.load(access_token.get('/api/rest/v2?method=vimeo.people.getInfo&format=json').body)
-      rescue ::Errno::ETIMEDOUT
-        raise ::Timeout::Error
-      end
-
       def request_phase
         options[:authorize_params] = {:permission => options[:scope]} if options[:scope]
         super
